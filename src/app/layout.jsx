3b76a3ceb3c02aaf4e5200c2c4611b59579c74fb
nilking23 <nlk@cowanandking.com>
2024-11-26 10:38:06 -0500

    Initial commit.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import '@/app/ui/global.css'; // Import global styles
+import { inter } from '@/app/ui/fonts'; // Import the Inter font
 
 export const metadata = {
   title: "MO-PCCO.R3 - NextJS Financial Dashboard",
@@ -10,8 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+          <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
   );
 }
